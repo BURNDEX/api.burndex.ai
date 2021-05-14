0e05a9677d6c7b5a5ef5f8b84bf08d48ca50792c
Justin Singh-Mohudpur <justin@justinsingh.me>
2021-05-13 19:43:00 -0700

    Fixed fire_path

--- R/globals.R
+++ R/globals.R
@@ -1,4 +1,4 @@
 bagged_mars <- get_mars_model()
 
-fire_path <- sf::read_sf(paste0("https://opendata.arcgis.com/datasets/",
-                                "f72ebe741e3b4f0db376b4e765728339_0.geojson"))
+fire_path <- paste0("https://opendata.arcgis.com/datasets/",
+                    "f72ebe741e3b4f0db376b4e765728339_0.geojson")
