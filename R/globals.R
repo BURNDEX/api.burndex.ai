5c2761e6910605ca0301789bdc16bef53c906071
Justin Singh-Mohudpur <justin@justinsingh.me>
2021-05-14 10:43:42 -0700

    Changed fire dataset

--- R/globals.R
+++ R/globals.R
@@ -8,4 +8,4 @@ library(climateR)
 bagged_mars <- get_mars_model()
 
 fire_path <- paste0("https://opendata.arcgis.com/datasets/",
-                    "f72ebe741e3b4f0db376b4e765728339_0.geojson")
+                    "5da472c6d27b4b67970acc7b5044c862_0.geojson")
